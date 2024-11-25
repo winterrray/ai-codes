2e03abcbd25b7ee985daff57df7e6600086858a9
Rajnandini <144838546+winterrray@users.noreply.github.com>
2024-11-25 08:06:47 +0530

    Update 4_astar.cpp

--- 4_astar.cpp
+++ 4_astar.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 using namespace std;
 
 vector<vector<int>> graph = {
@@ -13,43 +12,71 @@ vector<vector<int>> graph = {
     {10, 0, 0, 4, 2, 0}
 };
 
+struct Node {
+    int id;
+    int g;
+    int f;      // g + h
+};
+
 struct Compare{
-    bool operator()(pair<int, int> a,pair<int, int> b){
-        return a.second > b.second;
+    bool operator()(Node a, Node b){
+        return a.f > b.f;   // min heap (smallest f at top)
     }
 };
 
-void a_star(vector<int> h, int start, int end){
-    //define pq
-    priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
-    //visited array
-    vector<int> visited(graph.size(), -1);
-    //dist array
-    vector<int> dist(graph.size(), INT_MAX);
+void a_star(int h[], int start, int goal){
+    int n = graph.size();
+   
+    vector<bool> visited(n, false);     // visited array
+    vector<int> gArr(n, INT_MAX);          // distance array
+    vector<int> parent(n, -1);          // parent array
 
-    dist[start] = 0;
-    pq.push({start, h[start]});
+    priority_queue <Node, vector<Node>, Compare> pq;     // define pq
+
+    gArr[start] = 0;
+    pq.push({start, 0, h[start]});
 
     while(!pq.empty()){
-        int node = pq.top().first;
+        Node curr = pq.top();
         pq.pop();
 
-        if (visited[node] == 1) continue;
-        cout << node << " ";
-        visited[node] = 1;
+        if (visited[curr.id]) continue;
+        visited[curr.id] = true;
+        cout << curr.id << " ";
 
-        if(node == end){
-            cout << endl;
-            cout << "Reached Destination" << endl;
+        // goal check
+        if(curr.id == goal){
+            cout << "\nReached Destination" << endl;
+
+            cout << "Path costs: ";
+            for (int i = 0; i < n; i++) {
+                if (gArr[i] == INT_MAX)
+                    cout << "INF";
+                else
+                    cout << gArr[i] << " ";
+            }
+            cout << "\n";
+
+            // print path
+            cout << "Path: ";
+            vector<int> path;
+            for (int at = goal; at != 1; at = parent[at]) {
+                path.push_back(at);
+            }
+            for(int i = path.size()-1; i >= 0; i--) {
+                cout << path[i] << " -> ";
+            }
+            cout << "\n";
             return;
         }
        
-        for(int i = 0;i < graph[0].size();i++){
-            if(visited[i] == -1 && graph[node][i] != 0){
-                int cost = graph[node][i] + dist[node];
-                if(cost < dist[i]){
-                    pq.push({i, h[i] + cost});
-                    dist[i] = cost;
+        for (int next = 0; next < n; next++) {
+            if (graph[curr.id][next] == 1) {
+                int new_g = gArr[curr.id] + graph[curr.id][next];
+                if (new_g < gArr[next]) {
+                    gArr[next] = new_g;
+                    parent[next] = curr.id;
+                    pq.push({next, new_g, (new_g + h[next])});
                 }
             }
         }
@@ -59,9 +86,9 @@ void a_star(vector<int> h, int start, int end){
 }
 
 int main() {
-    vector<int> h = {5, 3, 4, 2, 6, 0};
-    int start = 0;
-    int end = 5;
+    int h[] = {5, 3, 4, 2, 6, 0};
+    // int start = 0;
+    // int end = 5;
     a_star(h, 0, 5);
     return 0;
 }
